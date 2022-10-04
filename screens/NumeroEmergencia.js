0a41d954c06a019be869ad7c7da8eb630eaba587
Mateo <mateoandraca@hotmail.com>
2022-10-04 14:43:56 -0300

    Parte 1 Numero de Emergencia

--- screens/NumeroEmergencia.js
+++ screens/NumeroEmergencia.js
@@ -1,10 +1,130 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useRef,useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import PhoneInput from 'react-native-phone-number-input';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function NumeroEmergencia() {
+const NumeroEmergencia = ({route,navigation}) => {
+  const{setNumEmergencia} = route.params;
+  const [value, setValue] = useState('');
+  const [countryCode, setCountryCode] = useState('');
+  const [formattedValue, setFormattedValue] = useState('');
+  const [valid, setValid] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const phoneInput = useRef(null);
+
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('numEmergencia', value)
+    } catch (e) {
+      // saving error
+    }
+  }
+
+  useEffect(() => {
+    if(valid==true){
+      storeData(value)
+      setNumEmergencia(value)
+    }
+  },[valid]);
+
   return (
-    <View>
-      <Text>NumeroEmergencia</Text>
-    </View>
-  )
-}
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <SafeAreaView style={styles.wrapper}>
+          {showMessage && (
+            <View style={styles.message}>
+              <Text>Country Code : {countryCode}</Text>
+              <Text>Value : {value}</Text>
+              <Text>Formatted Value : {formattedValue}</Text>
+              <Text>Valid : {valid ? 'true' : 'false'}</Text>
+            </View>
+          )}
+          <PhoneInput
+            ref={phoneInput}
+            defaultValue={value}
+            defaultCode="AR"
+            layout="first"
+            onChangeText={(text) => {
+              setValue(text);
+            }}
+            onChangeFormattedText={(text) => {
+              setFormattedValue(text);
+              setCountryCode(phoneInput.current?.getCountryCode() || '');
+            }}
+            countryPickerProps={{withAlphaFilter:true}}
+            disabled={disabled}
+            withDarkTheme
+            withShadow
+            autoFocus
+          />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              const checkValid = phoneInput.current?.isValidNumber(value);
+              setShowMessage(true);
+              setValid(checkValid ? checkValid : false);
+              setCountryCode(phoneInput.current?.getCountryCode() || '');
+              let getNumberAfterPossiblyEliminatingZero = phoneInput.current?.getNumberAfterPossiblyEliminatingZero();
+            }}>
+            <Text style={styles.buttonText}>Check</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lighter,
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 20,
+    height: 50,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#7CDB8A',
+    shadowColor: 'rgba(0,0,0,0.4)',
+    shadowOffset: {
+      width: 1,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 14,
+  },
+  redColor: {
+    backgroundColor: '#F57777'
+  },
+  message: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+});
+
+export default NumeroEmergencia;
--- screens/Principal.js
+++ screens/Principal.js
@@ -1,15 +1,33 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button} from 'react-native'
+import React ,{useEffect, useState}from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Principal({navigation, route}) {
+
+  const [numEmergencia,setNumEmergencia] = useState(null);
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('numEmergencia')
+      if(value !== null) {
+        setNumEmergencia(value)
+      }
+    } catch(e) {
+      // error reading value
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  },[]);
 
   return (
     <View style={{flex:1, alignItems: "center", justifyContent: "space-around"}}>
       <Button title='Qr' onPress={()=>navigation.navigate("Qr")} />
       <Button title='Video Favorito' onPress={()=>navigation.navigate("VideoFavorito")} />
-      <Button title='Numero Emergencia' onPress={()=>navigation.navigate("NumeroEmergencia")} />
+      <Button title='Numero Emergencia' onPress={()=>navigation.navigate("NumeroEmergencia",{setNumEmergencia})} />
       <Button title='Clima' onPress={()=>navigation.navigate("Clima")} />
-      <Button title='Contactos' onPress={()=>navigation.navigate("Contactos")} />
+      <Button title='Contactos' onPress={()=>navigation.navigate("Contactos",{numEmergencia})} />
     </View>
   )
 }
