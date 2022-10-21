b48f7713ec1f70f3d2cc52db2e9503c757971031
mateo16 <mateoandraca@hotmail.com>
2022-10-21 09:49:06 -0300

    ...

--- screens/VideoFavorito.js
+++ screens/VideoFavorito.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text ,TextInput} from 'react-native'
+import React, { useState } from 'react'
+import Video from 'react-native';
+import { StyleSheet, ImageBackground } from "react-native";
+import { WebView } from 'react-native-webview';
+
 
 export default function VideoFavorito() {
+    const [url,setUrl] = useState("");
+  
   return (
+    <>
     <View>
-      <Text>VideoFavorito</Text>
+      <TextInput style={styles.input} onChangeText={(text) => setUrl(text)}/>
     </View>
+          <WebView
+    style={{flex:1}}
+    javaScriptEnabled={true}
+    source={{uri: url}}
+/>
+    </>
+
+
+
   )
+  
 }
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
